75d1a3ce462a5f3184412eac63eea3c7b96a1bbb
phat <hongphat701@gmail.com>
2019-12-13 11:48:25 +0700

    lan 6

--- bt.cpp
+++ bt.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
 #include <math.h>
 using namespace std;
+bool ktshh(int n)
+{
+	int sum = 0;
+	for(int i = 1 ;i <= n/2 ;i++)
+	{
+		if(n%i == 0) sum+=i;
+	}
+	if(sum == n) return true;
+	return false;
+}
+void xuatshh(int arr[],int n)
+{
+	for(int i = 0 ; i < n; i++)
+	{
+		if(ktshh(arr[i])) cout<<arr[i]<<" ";
+	}
+}
+bool ktscp(int n)
+{
+	int result = sqrt(n);
+	return (result*result == n);
+}
+void xuatscp(int arr[],int n)
+{
+	for(int i = 0 ; i < n; i++)
+	{
+		if(ktscp(arr[i])) cout<<arr[i]<<" ";
+	}
+}
 bool ktsnt(int n)
 {
 	if(n < 2) return false;
@@ -25,6 +54,6 @@ int main(){
 	{
 		cout<<"Pt["<<i<<"]="; cin>>arr[i];
 	}	
-	xuatsnt(arr,n);
+	xuatshh(arr,n);
 	return 0;
 }
